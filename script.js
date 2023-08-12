0421566eb09d85b3e8653f3296280ad64f6981d0
Rahul Barodia <rahulbarodia28@gmail.com>
2023-08-13 02:00:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const backToTopButton = document.getElementById("backToTopButton");
+backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
+
